15ffac11b1e039efa8379a3af3041648781a8a48
Tadas Sipavicius <tasipas@gmail.com>
2022-11-14 00:26:28 +0200

    make it betterasadasdaasdad

--- src/Config/logging.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const getTimeStamp = () => {
-    return new Date().toISOString();
-};
-const info = (namespace, message, object) => {
-    if (object) {
-        console.log(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`, object);
-    }
-};
